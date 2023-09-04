f0b382b7a07533df814c3608da3683a8d42704fc
eduardoandd <eduardo.andrade.dev@gmail.com>
2023-09-03 23:09:58 -0300

    first commit

---
+++ 1_menu.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.markdown('# FIFA 23')
+st.sidebar.markdown('Desenvolvido por [mim](https://github.com/eduardoandd)')
+
+btn = st.button('Dados retirados do Kaggles')
+
+if btn:
+    
